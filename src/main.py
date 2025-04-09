5296e6545591ee26d462efea33abbab33520c40e
Max <maxdowner96@gmail.com>
2025-04-09 18:17:22 +0100

    add extract to main function

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from extract import extract
+
+# import and transform the data
+# probably using pandas
+
+def main():
+    extract()
+
+# structure
+# input is 28*28 grayscale pixels(0-255?), thus 784 input neurons
+# 2 hidden layers of 16
+# 10 output neurons
+# starting to set in that this may be a more laborious task than anticipated, but I guess that gives me motivation to learn how to code it efficiently
+# maybe not, numpy has good functions
+
+# structure - random
+# way to randomly assign initial weights for each layer
+# '''
+# 
+# '''
+# create
+# a matrix with size ? and populate it with random numbers between ? and ?
+# initial_weights = np.random.rand(raw_training_data.shape()) # need to add additional params to define the random numbers, and consult style guide for variables
+
+# np.multiply(mmatrix1,mmatrix2)
+
+
+
+
+
