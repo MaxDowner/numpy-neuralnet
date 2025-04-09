2c2df8a88a4e38bb3642a6fec4958d1f61a43129
Max <maxdowner96@gmail.com>
2025-04-09 20:26:37 +0100

    add a test to extract, checking that the returned objects are dataframes

--- test/test_extract.py
+++ test/test_extract.py
@@ -1,5 +1,14 @@
-# check number of rows, number of columns, whether first row follows appropriate pattern
+# check number of rows, number of columns, whether first row follows appropriate pattern, check type(s) 
+from src.extract import extract
+import pytest
+import pandas as pd
 
-class test_class():
-    def test_extract(self):
-        pass
+# put tests in a class
+
+@pytest.mark.skip(reason="extract takes a long time")
+def test_extract():
+    # arrange
+    # act
+    result = extract()
+    # assert
+    assert (isinstance(result[0], pd.DataFrame) & isinstance(result[1], pd.DataFrame))
