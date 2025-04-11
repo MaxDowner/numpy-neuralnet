dfeb6ac2b2492b1032fd8f0f1881f6a87d426efb
Max <maxdowner96@gmail.com>
2025-04-11 18:44:51 +0100

    add simple, single line docstrings

--- src/extract.py
+++ src/extract.py
@@ -6,6 +6,7 @@
 import pandas as pd
 
 def extract():
+    ''' change the csvs into dataframes and return them in a tuple '''
     raw_test_data = pd.read_csv('../dataset/mnist_test.csv')
     raw_training_data = pd.read_csv('../dataset/mnist_train.csv')
     return raw_test_data, raw_training_data
---
+++ src/loss_function.py
@@ -0,0 +1,6 @@
+# Iirc its the sum (ITS THE AVERAGE) of the square of the differences between the wanted values (0, 1) for each digit, 
+# and the actual values obtained minus a bias (for improved edge detection)
+
+def loss_function():
+    ''' define and calculate the function the neural network is looking to minimise '''
+    pass
